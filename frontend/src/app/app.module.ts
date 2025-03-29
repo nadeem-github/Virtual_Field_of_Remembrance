9d2f67d3be949507c0e1c10a61a3b01bdc33b119
nadeem-github <m.nadeempatel@gmail.com>
2025-03-29 17:37:18 +0530

    push all pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeListingComponent } from './pages/home-listing/home-listing.component';
+import { AddFormComponent } from './pages/add-form/add-form.component';
+import { EditFormComponent } from './pages/edit-form/edit-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeListingComponent,
+    AddFormComponent,
+    EditFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/pages/home-listing/home-listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-listing',
+  templateUrl: './home-listing.component.html',
+  styleUrls: ['./home-listing.component.scss']
+})
+export class HomeListingComponent {
+
+}
