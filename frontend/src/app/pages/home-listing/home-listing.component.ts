c261aab87a6c6b58d9968ae6135db9063e334ab0
nadeem-github <m.nadeempatel@gmail.com>
2025-04-17 20:00:36 +0530

    add draggble image

--- frontend/src/app/pages/home-listing/home-listing.component.ts
+++ frontend/src/app/pages/home-listing/home-listing.component.ts
@@ -12,8 +12,8 @@ export class HomeListingComponent {
   isLoading = true;
   remembrances: any[] = [];
   requestData = {};
-  // baseURL = 'http://localhost:8002/storage/images/';
-  baseURL = 'https://api.familiesofveterans.org.au/storage/images/';
+  baseURL = 'http://localhost:8002/storage/images/';
+  // baseURL = 'https://api.familiesofveterans.org.au/storage/images/';
   // https://api.familiesofveterans.org.au/api/admin/fetch-remebrance
   selectedRemembrance: any = null;
 
@@ -31,7 +31,7 @@ export class HomeListingComponent {
     this.virtualFielsService.getRemembrances(this.requestData).subscribe(response => {
       if (response.success) {
         this.remembrances = response.data;
-        console.log(this.remembrances);
+        // console.log(this.remembrances);
       }
     });
   }
@@ -44,9 +44,48 @@ export class HomeListingComponent {
   showDetails(remembrance: any): void {
     this.selectedRemembrance = remembrance;
   }
-  
+
   hideDetails(): void {
     this.selectedRemembrance = null;
   }
-  
+
+  backgroundPositionX = 0;
+  backgroundPositionY = 0;
+  isDragging = false;
+  startX = 0;
+  startY = 0;
+
+  onMouseDown(event: MouseEvent) {
+    this.isDragging = true;
+    this.startX = event.clientX - this.backgroundPositionX;
+    this.startY = event.clientY - this.backgroundPositionY;
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.isDragging) return;
+
+    this.backgroundPositionX = event.clientX - this.startX;
+    this.backgroundPositionY = event.clientY - this.startY;
+  }
+
+  onMouseUp() {
+    this.isDragging = false;
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.isDragging = true;
+    this.startX = event.touches[0].clientX - this.backgroundPositionX;
+    this.startY = event.touches[0].clientY - this.backgroundPositionY;
+  }
+
+  onTouchMove(event: TouchEvent) {
+    if (!this.isDragging) return;
+    this.backgroundPositionX = event.touches[0].clientX - this.startX;
+    this.backgroundPositionY = event.touches[0].clientY - this.startY;
+  }
+
+  onTouchEnd() {
+    this.isDragging = false;
+  }
+
 }
